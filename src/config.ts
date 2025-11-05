2a274732d8fc1e81e291511fe44596b8ae1fd9b9
Yuma Shintani <yuu7taku@icloud.com>
2025-11-05 19:00:56 +0900

    enable generate og image

--- src/config.ts
+++ src/config.ts
@@ -160,7 +160,7 @@ export const siteConfig: SiteConfig = {
 		enable: true, // 启用目录功能
 		depth: 3, // 目录深度，1-6，1 表示只显示 h1 标题，2 表示显示 h1 和 h2 标题，依此类推
 	},
-	generateOgImages: false, // 启用生成OpenGraph图片功能,注意开启后要渲染很长时间，不建议本地调试的时候开启
+	generateOgImages: true, // 启用生成OpenGraph图片功能,注意开启后要渲染很长时间，不建议本地调试的时候开启
 	favicon: [
 		// 留空以使用默认 favicon
 		// {
