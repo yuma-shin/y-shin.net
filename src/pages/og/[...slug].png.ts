b2d31402f5f9514f3d0efc2fa3193890685af962
Yuma Shintani <yuu7taku@icloud.com>
2025-12-05 20:18:40 +0900

    fix favicon file name

--- src/pages/og/[...slug].png.ts
+++ src/pages/og/[...slug].png.ts
@@ -109,7 +109,7 @@ export async function GET({
 	const avatarBuffer = fs.readFileSync(`./src/${profileConfig.avatar}`);
 	const avatarBase64 = `data:image/png;base64,${avatarBuffer.toString("base64")}`;
 
-	let iconPath = "./public/favicon/favicon.ico";
+	let iconPath = "./public/favicon/favicon-dark-192.png";
 	if (siteConfig.favicon.length > 0) {
 		iconPath = `./public${siteConfig.favicon[0].src}`;
 	}
