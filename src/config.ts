3a3242e6379cc18cbe7a4ce923551bcdb70b6f44
Yuma Shintani <yuu7taku@icloud.com>
2025-12-14 02:36:42 +0900

    update

--- src/config.ts
+++ src/config.ts
@@ -201,16 +201,16 @@ export const siteConfig: SiteConfig = {
 export const fullscreenWallpaperConfig: FullscreenWallpaperConfig = {
 	src: {
 		desktop: [
-			"/assets/desktop-banner/wallpaper.png",
-			"/assets/desktop-banner/wallpaper-3.png",
+			"/assets/desktop-banner/wallpaper.webp",
+			"/assets/desktop-banner/wallpaper-3.webp",
 			"/assets/desktop-banner/wallpaper-2.webp",
 			"/assets/desktop-banner/wallpaper-4.webp",
 			"/assets/desktop-banner/wallpaper-5.webp",
 			"/assets/desktop-banner/wallpaper-6.webp",
 		], // 桌面横幅图片
 		mobile: [
-			"/assets/desktop-banner/wallpaper.png",
-			"/assets/desktop-banner/wallpaper-3.png",
+			"/assets/desktop-banner/wallpaper.webp",
+			"/assets/desktop-banner/wallpaper-3.webp",
 			"/assets/desktop-banner/wallpaper-2.webp",
 			"/assets/desktop-banner/wallpaper-4.webp",
 			"/assets/desktop-banner/wallpaper-5.webp",
