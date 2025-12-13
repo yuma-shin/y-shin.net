bdb98bfed9ca2921b189811ea147e65fc7d18727
Yuma Shintani <yuu7taku@icloud.com>
2025-12-14 03:02:57 +0900

    remove umami

--- src/config.ts
+++ src/config.ts
@@ -674,12 +674,3 @@ export const widgetConfigs = {
 	fullscreenWallpaper: fullscreenWallpaperConfig,
 	//pio: pioConfig, // 添加 pio 配置
 } as const;
-
-export const umamiConfig = {
-	enabled: false, // 是否显示Umami统计
-	apiKey: import.meta.env.UMAMI_API_KEY || "api_xxxxxxxx", // API密钥优先从环境变量读取，否则使用配置文件中的值
-	baseUrl: "https://api.umami.is", // Umami Cloud API地址
-	scripts: `
-<script defer src="XXXX.XXX" data-website-id="ABCD1234"></script>
-  `.trim(), // 上面填你要插入的Script,不用再去Layout中插入
-} as const;
