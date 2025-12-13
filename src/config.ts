170340ac0abfe1d7107901d85014a7f22069a2b0
Yuma Shintani <yuu7taku@icloud.com>
2025-12-14 02:07:42 +0900

    update

--- src/config.ts
+++ src/config.ts
@@ -183,15 +183,17 @@ export const siteConfig: SiteConfig = {
 
 	// 字体配置
 	font: {
-		// 注意：自定义字体需要在 src/styles/main.css 中引入字体文件
-		// 注意：字体子集优化功能目前仅支持 TTF 格式字体,开启后需要在生产环境才能看到效果,在Dev环境下显示的是浏览器默认字体!
 		asciiFont: {
-			// 英文字体 - 优先级最高
-			// 指定为英文字体则无论字体包含多大范围，都只会保留 ASCII 字符子集
 			fontFamily: "ZenMaruGothic-Medium",
 			fontWeight: "400",
 			localFonts: ["ZenMaruGothic-Medium.ttf"],
-			enableCompress: false, // 启用字体子集优化，减少字体文件大小
+			enableCompress: false,
+		},
+		cjkFont: {
+			fontFamily: "ZenMaruGothic-Medium",
+			fontWeight: "400",
+			localFonts: ["ZenMaruGothic-Medium.ttf"],
+			enableCompress: false,
 		}
 	},
 	showLastModified: false, // 控制“上次编辑”卡片显示的开关
--- src/types/config.ts
+++ src/types/config.ts
@@ -90,14 +90,12 @@ export type SiteConfig = {
 			localFonts: string[];
 			enableCompress: boolean;
 		};
-		/*
 		cjkFont: {
 			fontFamily: string;
 			fontWeight: string | number;
 			localFonts: string[];
 			enableCompress: boolean;
 		};
-		*/
 	};
 
 	// 添加bangumi配置
