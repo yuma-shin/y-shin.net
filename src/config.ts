8ff221ef9c14490a9486c1da7ebcc06f2dbcf720
Yuma Shintani <yuu7taku@icloud.com>
2025-12-14 02:46:46 +0900

    change time zone

--- src/config.ts
+++ src/config.ts
@@ -18,7 +18,7 @@ import { LinkPreset } from "./types/config";
 
 // 定义站点语言
 const SITE_LANG = "ja"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
-const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UTC+8
+const SITE_TIMEZONE = 9; //设置你的网站时区 from -12 to 12 default in UTC+8
 export const siteConfig: SiteConfig = {
 	title: "Yumrith",
 	subtitle: "Yuma Shintani",
